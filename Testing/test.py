ad0e726410cbb13fabbb1588db5de645cc00779d
Spinichef <198487667+Spinichef@users.noreply.github.com>
2025-02-10 19:10:19 +0100

    test
    Tested a function

--- Testing/test.py
+++ Testing/test.py
@@ -1,1 +1,12 @@
+# Variables used in the example ``if`` statements
+a = 4
+b = 5
 
+# Basic comparisons
+if a <= b:
+    print("a is less than b")
+
+if a >= b:
+    print("a is greater than b")
+
+print("Done")
