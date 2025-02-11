9744955204539f8070d1bd1efe76cb423581186d
Spinichef <198487667+Spinichef@users.noreply.github.com>
2025-02-11 21:52:41 +0100

    lots of training
    Loops

---
+++ Lab 05 - Loopy Lab/lab_05.py
@@ -0,0 +1,72 @@
+def main():
+
+    for i in range(10):
+        print("*", end=" ")
+    print()
+    for i in range(5):
+        print("*", end=" ")
+    print()
+    for i in range(20):
+        print("*", end=" ")
+
+def second():
+    for i in range(10):
+        for j in range(10):
+            print("*", end=" ")
+        print()
+
+def third():
+    for i in range(10):
+        for j in range(5):
+            print("*", end=" ")
+        print()
+
+def fourth():
+    for i in range(5):
+        for j in range(20):
+            print("*", end=" ")
+        print()
+
+def fifth():
+    for i in range(10):
+        for j in range(10):
+            print(j, end=" ")
+        print()
+
+def sixth():
+    for i in range(10):
+        for j in range(10):
+            print(i, end=" ")
+        print()
+
+def seventh():
+    for i in range(10):
+        for j in range(i + 1):
+            print(j, end=" ")
+        print()
+
+def eighth():
+    for i in range(10):
+        for j in range(i):
+            print(" ", end=" ")
+        for j in range(10 - i):
+            print(j, end=" ")
+        print()
+
+def ninth():
+    for i in range(1, 10):
+        for j in range(1, 10):
+            if i * j < 10:
+                print(" ", end="")
+            print(i * j, end=" ")
+        print()
+
+def tenth():
+    for i in range(10):
+        for j in range(10 - i):
+            print(" ", end=" ")
+        for j in range(1, i + 1):
+            print(j, end=" ")
+        for j in range(i - 1, 0, -1):
+            print(j, end=" ")
+        print()
--- Testing/test.py
+++ Testing/test.py
@@ -1,10 +1,18 @@
-user_name = (input("What is your name? "))
-if user_name.lower() == "jonas":
-    print("You have a good name!")
-elif user_name.lower() == "bene" or user_name.lower() == "benedikt":
-    print("You have a bad name!")
-elif user_name.lower() == "fickdich" or user_name.lower() == "fick dich":
-    for i in range(5):
-        print("Bene sei leise.")
-else:
-    print("You have an ok name!")
+def test():
+
+    user_name = (input("What is your name? "))
+    if user_name.lower() == "jonas":
+        print("You have a good name!")
+    elif user_name.lower() == "bene" or user_name.lower() == "benedikt":
+        print("You have a bad name!")
+    elif user_name.lower() == "fickdich" or user_name.lower() == "fick dich":
+        for i in range(5):
+            print("Bene sei leise.")
+    else:
+        print("You have an ok name!")
+
+def test2():
+    for i in range(10):
+        for j in range(10):
+            print("Bene sei leise.")
+test2()
