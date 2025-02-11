fffd1e399fc18423bc14c2740dd4a2cb8d271ade
Spinichef <198487667+Spinichef@users.noreply.github.com>
2025-02-11 14:15:56 +0100

    funny update
    goofy shenanigans

--- Testing/test.py
+++ Testing/test.py
@@ -1,12 +1,10 @@
-# Variables used in the example ``if`` statements
-a = 4
-b = 5
-
-# Basic comparisons
-if a <= b:
-    print("a is less than b")
-
-if a >= b:
-    print("a is greater than b")
-
-print("Done")
+user_name = (input("What is your name? "))
+if user_name.lower() == "jonas":
+    print("You have a good name!")
+elif user_name.lower() == "bene" or user_name.lower() == "benedikt":
+    print("You have a bad name!")
+elif user_name.lower() == "fickdich" or user_name.lower() == "fick dich":
+    for i in range(5):
+        print("Bene sei leise.")
+else:
+    print("You have an ok name!")
